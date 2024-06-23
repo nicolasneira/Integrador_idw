818753e8eeab0406f219a654ecfd20599edd75ac
nicolasneira <71415090+nicolasneira@users.noreply.github.com>
2024-06-23 17:02:14 -0300

    Add files via upload

---
+++ js/consulta.js
@@ -0,0 +1,12 @@
+function busqueda(){
+	
+	var y=document.getElementById("dni").value;
+	
+	if (y == 42633309){
+		document.getElementById("resp").innerHTML="Verificacion";
+		alert("hola");
+	}else{
+		alert("pop");
+	}
+
+}
